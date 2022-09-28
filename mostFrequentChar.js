668a5427725b2477f2e035984998f6aa585b68ab
Charles Jarvis <csjarvis43@gmail.com>
2022-09-28 14:55:26 -0600

    added structy solution

--- mostFrequentChar.js
+++ mostFrequentChar.js
@@ -7,17 +7,41 @@ const mostFrequentChar = (str) => {
     let result = ''
 
     for (let char of str) {
-        fq[char] = (fq[char] || 0) + 1
+        fq[char] = (fq[char] || 0) + 1;
     }
 
-    for (key in fq) {
+    for (let key in fq) {
         if (fq[key] > max) {
             result = key;
             max = fq[key];
         }
     }
 
-    return result
+    return result;
 }
 
 console.log(mostFrequentChar('bookkeeper'))
+
+
+// Structy solution
+
+const mostFrequentCharStructy = (s) => {
+    const count = {};
+
+    for (let char of s) {
+        if (!(char in count)) {
+            count[char] = 0;
+        }
+
+        count[char] += 1;
+    }
+
+    let best = null;
+    for (let char of s) {
+        if(best === null || count[char] > count[best]) {
+            best = char;
+        }
+    }
+
+    return best
+}
