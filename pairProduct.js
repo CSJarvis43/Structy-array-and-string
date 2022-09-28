8324363763139cb7f93166182f0ef0dc672ebe74
Charlie Jarvis <csjarvis43@gmail.com>
2022-09-28 16:37:18 -0600

    added structy solution

--- pairProduct.js
+++ pairProduct.js
@@ -46,3 +46,23 @@ const pairProduct = (nums, target) => {
 
 console.log(pairProduct([2, 3, 5, 4, 1], 8))
 
+//Structy solution
+
+const pairProductStructy = (numbers, targetProduct) => {
+    const previousNums = {};
+
+    for (let i = 0; i < numbers.length; i ++) {
+
+        const num = numbers[i];
+        const complement = targetProduct / num;
+
+        if (complement in previousNums) {
+            return [i, previousNums[complement]];
+        }
+
+
+
+        previousNums[num] = i;
+    }
+}
+
