e6ebd20fb1512aec568665e39a6bd49925cb6fa4
Charles Jarvis <csjarvis43@gmail.com>
2022-09-28 15:23:30 -0600

    charlie's solution to pairSum

---
+++ pairSum.js
@@ -0,0 +1,17 @@
+// Charlie's initial solution
+
+function pairSum(numbers, targetSum) {
+    
+    let previous = {};
+
+    for (let i = 0; i < numbers.length; i++) {
+        const complement = targetSum - numbers[i];
+        if (previous[complement] >= 0) {
+            return [previous[complement], i];
+        } else {
+            previous[numbers[i]] = i;
+        }
+    }
+}
+
+console.log(pairSum([4, 7, 9, 2, 5, 1], 3))
