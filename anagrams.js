28f79c2c8b799e421045c5e1a05c6f9a8e1c8c4b
Charles Jarvis <csjarvis43@gmail.com>
2022-09-28 14:25:11 -0600

    added structy solution

--- anagrams.js
+++ anagrams.js
@@ -23,3 +23,33 @@ function anagrams(str1, str2) {
 
 
 console.log(anagrams('monkeyswrite', 'newyorktimes'))
+
+// Structy walkthrough solution
+
+function anagramsSolution(s1, s2) {
+    const count = {};
+
+    for (let char of s1) {
+        if (!(char in count)) {
+            count[char] = 0
+        }
+
+        count[char] += 1;
+    } 
+
+    for (let char of s2) {
+        if (char in count) {
+            count[char] -= 1
+        } else return false
+    }
+
+    for(let char in count) {
+        if (count[char] !== 0) {
+            return false
+        }
+    }
+    
+    return true
+}
+
+console.log(anagramsSolution('monkeyswrite', 'newyorktimes'))
