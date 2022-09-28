eb5f910578389b9c8f97ad0a5cc3789c53a4149b
Charlie Jarvis <csjarvis43@gmail.com>
2022-09-28 16:31:06 -0600

    charlie's solution finished

--- pairProduct.js
+++ pairProduct.js
@@ -1,5 +1,27 @@
+// Charlie's solution
+
 const pairProduct = (nums, target) => {
 
 
+    const prevNums = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if (target % nums[i] === 0) {
+            const num = nums[i];
+            const complement = target / num;
+
+            if (prevNums[complement] >= 0) {
+                return [prevNums[complement], i];
+            } else {
+                prevNums[num] = i;
+            }
+        }
+    }
+
     
 }
+
+
+
+console.log(pairProduct([2, 3, 5, 4, 1], 8))
+
