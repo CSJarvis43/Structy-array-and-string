630316bc04f960d4a5711c58eb4384adc04801f6
Charles Jarvis <csjarvis43@gmail.com>
2022-09-29 10:56:16 -0600

    added structy solution

--- fiveSort.js
+++ fiveSort.js
@@ -21,3 +21,21 @@ const fiveSort = (nums) => {
 }
 
 console.log(fiveSort([1,5,2,5,2,3,4,5,5]))
+
+// Structy solution
+
+const fiveSortStructy = (nums) => {
+    let i = 0;
+    let j = nums.length - 1;
+
+    while (i <= j) {
+        if (nums[j] === 5) {
+            j -=1;
+        } else if (nums[i] === 5) {
+            [ nums [i], nums[j] ] = [ nums[j], nums[i] ];
+            i++;
+        } else i++;
+    }
+
+    return nums;
+}
